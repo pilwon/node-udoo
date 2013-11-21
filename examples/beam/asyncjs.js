ec4f77d2a1a1986d07a370455fc42e01355f1242
Pilwon Huh <pilwon@gmail.com>
2013-11-20 19:27:26 -0500

    Added beam examples.

---
+++ examples/beam/asyncjs.js
@@ -0,0 +1,57 @@
+/*
+ * examples/beam/asyncjs.js
+ */
+
+'use strict';
+
+var udoo = require('../..');
+
+var DELAY_LED = 5,
+    DELAY_LOOP = 500,
+    PIN_NUMBER_FROM = 2,
+    PIN_NUMBER_TO = 15;
+
+var leds = [],
+    slowDown = false,
+    i;
+
+function loop(cb) {
+  var tasks = [];
+
+  leds.forEach(function (led) {
+    tasks.push(function (cb) {
+      udoo.async.series([
+        function (cb) {
+          led.setHigh(cb);
+        },
+        function (cb) {
+          setTimeout(cb, (slowDown ? 7 : 1) * DELAY_LED);
+        },
+        function (cb) {
+          led.setLow(cb);
+        }
+      ], cb);
+    });
+  });
+
+  tasks.push(function (cb) {
+    leds.reverse();
+    slowDown = !slowDown;
+    setTimeout(cb, DELAY_LOOP);
+  });
+
+  udoo.async.series(tasks, cb);
+}
+
+for (i = PIN_NUMBER_FROM; i <= PIN_NUMBER_TO; ++i) {
+  leds.push(udoo.outputPin(i));
+}
+
+udoo.async.series([
+  udoo.reset,
+  function (cb) {
+    udoo.async.forever(loop, cb);
+  }
+], function (err) {
+  throw err;
+})
---
+++ examples/beam/promise.js
@@ -0,0 +1,45 @@
+/*
+ * examples/beam/promise.js
+ */
+
+'use strict';
+
+var udoo = require('../..');
+
+var DELAY_LED = 5,
+    DELAY_LOOP = 500,
+    PIN_NUMBER_FROM = 2,
+    PIN_NUMBER_TO = 15;
+
+var leds = [],
+    slowDown = false,
+    i;
+
+function loop() {
+  var promise = udoo.Q();
+
+  leds.forEach(function (led) {
+    promise = promise.then(function () {
+      return led
+        .setHigh()
+        .then(function () {
+          return udoo.Q.delay((slowDown ? 7 : 1) * DELAY_LED);
+        })
+        .then(function () {
+          return led.setLow();
+        });
+    });
+  });
+
+  promise.done(function () {
+    leds.reverse();
+    slowDown = !slowDown;
+    setTimeout(loop, DELAY_LOOP);
+  });
+}
+
+for (i = PIN_NUMBER_FROM; i <= PIN_NUMBER_TO; ++i) {
+  leds.push(udoo.outputPin(i));
+}
+
+udoo.reset().done(loop);
